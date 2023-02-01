7aeceda4892a1194e69bcafe7f7aea74a4d84fc8
Nikita Permiakou <naperm@gmail.com>
2023-02-01 04:41:57 +0300

    update

--- Code Sharing Platform/task/src/platform/DBRepository.java
+++ Code Sharing Platform/task/src/platform/DBRepository.java
@@ -3,7 +3,8 @@ package platform;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
-public interface DBRepository extends CrudRepository<Code, String> {
+public interface DBRepository extends CrudRepository<Code, Long> {
 }
